6081f87f89bb270d2b7a5571349dec1227dd4117
polar <polar@dona.one>
2025-08-25 08:11:01 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 class FirebaseConfig {
     constructor() {
         this.config = {
-            apiKey: process.env.FIREBASE_API_KEY || "AIzaSyBqJJQqJQqJQqJQqJQqJQqJQqJQqJQqJQq",
+            apiKey: process.env.FIREBASE_API_KEY || "AIzaSyBYyJuTAkVPB_65NqSeca6IueMoMO1iPzs",
             authDomain: process.env.FIREBASE_AUTH_DOMAIN || "polars-shack.firebaseapp.com",
             projectId: process.env.FIREBASE_PROJECT_ID || "polars-shack",
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "polars-shack.appspot.com",
-            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "123456789012",
-            appId: process.env.FIREBASE_APP_ID || "1:123456789012:web:abcdefghijklmnopqr"
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "polars-shack.firebasestorage.app",
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "205388124682",
+            appId: process.env.FIREBASE_APP_ID || "1:205388124682:web:2b0b1605517e0c6c53c5f3"
         };
         
         this.initializeFirebase();
